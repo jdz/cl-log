3ad326ddc2d49fbc609e36f70bb037824a908445
Jānis Džeriņš <lisp@jonis.lv>
2024-06-06 10:19:20 +0300

    Simplify syslog logging
    Extra syslog feature support needs to be carefully thought out.  We
    kind-of tried to support `ident` parameter, but then there is this in
    the man page:
    
      The argument ident in the call of openlog() is probably stored as-is.
      Thus, if the string it points to is changed, syslog() may start
      prepending the changed string, and if the string it points to ceases
      to exist, the results are undefined.  Most portable is to use a string
      constant.
    
    Not sure if there is a way to guarantee a string will not be moved by
    GC.  So far we've probably been just lucky -- maybe we've only used
    literal strings and those are put in "static space"?
    
    Also the use of `option` parameter (we used PID) might have been wrong.
    
    For now just use `syslog` function which will automatically use
    `openlog` and use program name as `ident`.

--- syslog.lisp
+++ syslog.lisp
@@ -32,40 +32,8 @@
       `(cl-log:log-manager-message *manager* ,category "~A" ,description)
       `(cl-log:log-manager-message *manager* ,category ,description ,@args)))
 
-;;; It only makes sense to have one of these -- we cannot have multiple
-;;; instances of SYSLOG-MESSENGER with different `ident', `facility' or
-;;; `option' values.  Currently only `ident' can be specified.
 (defclass messenger (cl-log:base-messenger)
-  ((ident
-    :initarg :ident
-    :reader syslog-messenger-ident)))
-
-(defvar %syslog-messenger nil)
-
-(defmethod initialize-instance :after ((instance messenger)
-                                       &key ident)
-  (let ((old-messenger %syslog-messenger))
-    (cond (old-messenger
-           (let ((old-ident (syslog-messenger-ident old-messenger))
-                 (class (class-name old-messenger)))
-             (if (equal ident old-ident)
-                 (warn "~A already started." class)
-                 (warn "~A with conflicting IDENT (~A) already started."
-                       class old-ident))))
-          (ident
-           ;; XXX: Options are hard coded, default facility (LOG_USER) is used.
-           (sb-posix:openlog ident sb-posix:log-pid))
-          (t
-           (error "Cannot create SYSLOG-MESSENGER without :IDENT.")))))
-
-;;; Best-effort own foot shooting prevention.
-(defclass closed-syslog-messenger (messenger)
   ())
-
-(defmethod cl-log:stop-messenger :after ((messenger messenger) &key)
-  (change-class messenger 'closed-syslog-messenger)
-  (sb-posix:closelog)
-  (setf %syslog-messenger nil))
 
 (defun syslog-priority (category)
   (ecase category
@@ -85,10 +53,6 @@
                      (apply #'format nil
                             (cl-log:message-description message)
                             (cl-log:message-arguments message)))))
-
-(defmethod cl-log:messenger-send-message ((messenger closed-syslog-messenger)
-                                          (message t))
-  (error "Cannot log message - syslog closed."))
 
 (defun start-messenger (&rest initargs)
   (apply #'cl-log:start-messenger 'messenger
